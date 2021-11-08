762795644f0b0707d7a1c93db122f42dca75464b
ABHINAV PATHAK <87684426+GhostXBeast@users.noreply.github.com>
2021-11-08 12:21:36 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,34 +3,34 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from config import BOT_USERNAME
 
 
-@Client.on_message(filters.command(["start", "start@Darkop_bot"]) & filters.private & ~filters.channel)
+@Client.on_message(filters.command(["start", "start@Regaltos_X1_Bot"]) & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
         text="**Hello 👋🏻 {}!**\n\nI **Can Play Music In Voice Chats of Telegram Groups.**I Have A **lot of cool feature that will amaze You!**\n\n**Click /cmdlist For More Help On My Usage ❤**".format(message.from_user.mention),
         reply_markup=InlineKeyboardMarkup(
             [[
-            InlineKeyboardButton("➕ Add To Your Group ➕", url="https://t.me/Darkop_bot?startgroup=true")
+            InlineKeyboardButton("➕ Add To Your Group ➕", url="https://t.me/Regaltos_X1_Bot?startgroup=true")
             ],[
-            InlineKeyboardButton("💬 Group", url="https://t.me/BotMusics"),
-            InlineKeyboardButton("Commands 🛠", url="https://telegra.ph/Music-Bot-05-07")
+            InlineKeyboardButton("💬 Group", url="https://t.me/Regaltos_Botz_support"),
+            InlineKeyboardButton("🛠 Updates", url="https://t.me/Regaltos_Botz")
             ]]
         ),
         disable_web_page_preview=True
     )
         
-@Client.on_message(filters.command(["start", "start@Darkop_bot"]) & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command(["start", "start@Regaltos_X1_Bot"]) & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
     await message.reply_text(
         text="**Music Bot Is Online ✅**",
         reply_markup=InlineKeyboardMarkup(
             [[
-            InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/MusicBotSupports")
+            InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/Regaltos_botz_Support")
             ]]
         )
     )
 
 
-@Client.on_message(filters.command(["cmdlist", "start@Darkop_bot"]) & filters.private & ~filters.channel)
+@Client.on_message(filters.command(["cmdlist", "start@Regaltos_X1_Bot"]) & filters.private & ~filters.channel)
 async def cmdlist(_, message: Message):
     await message.reply_text(
         text="""**Group Music Bot : Help Menu**
@@ -41,14 +41,12 @@ __× First Add Me To Your Group..
 **🏷 Common Commands.**
 
 • `/play` - Song Name : __Plays Via Youtube__
-• `/dplay` - Song Name : __Play Via Deezer__
 • `/splay` - Song Name : __Play Via Jio Saavn__
 • `/playlist` - __Show now playing list__
 • `/current` - __Show now playing__
 
 • `/song` - Song Name : __Get The Song From YouTube__
 • `/vid` - Video Name : __Get The Video From YouTube__
-• `/deezer` - song name : __download songs you want quickly via deezer__
 • `/saavn` - song name : __download songs you want quickly via saavn__
 • `/search` - YouTube Title : __(Get YouTube Search Query)__
 
@@ -58,9 +56,7 @@ __× First Add Me To Your Group..
 • `/pause` : __Pause Playing Music__
 • `/resume` : __Resume Playing Music__
 • `/end` : __Stops playing Music__
-• `/reload` : __Reloads Admin List__
-• `/userbotjoin` : __Assistant Joins The Group__
-• `/userbotleave` : __Assistant Leaves From The Group.__""",
+• `/reload` : __Reloads Admin List__""",
         reply_markup=InlineKeyboardMarkup(
               [[
               InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/MusicBotSupports")
